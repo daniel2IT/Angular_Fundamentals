fdeb12112f3f2cbbd8c2e0f00b0fe3b783bc5f3e
Daniel <Daniel.Vaskevic@wealth-dynamix.com>
2021-02-24 17:45:43 +0200

    Project<Hello World>
    Project Start <Hello World>

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { EventsAppComponent } from './events-app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    EventsAppComponent
+  ],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
---
+++ src/app/events/events-list.components.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'
+
+@Component({
+ selector: 'events-list',
+ template:`
+  <div>
+    <h1Upcoming Angular Events</h1>
+  </div>
+  `
+})
+
+
+export class EventsListComponent{
+
+}
