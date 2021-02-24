c8c5e8a80f77641be2a07b62ef4a26942d620a35
Daniel <Daniel.Vaskevic@wealth-dynamix.com>
2021-02-24 18:14:50 +0200

    Simple Start
    - Added Image
    - Added Global Styles

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h2>Hello Worldddddddddddd</h2>'
+  template: `<h2>Hello Worldddddddddddd</h2>
+  <img src="/assets/images/angular.png"/>`
 })
 export class EventsAppComponent {
   title = 'BigPic';
--- src/app/events/events-list.components.ts
+++ src/app/events/events-list.components.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core'
   <div>
     <h1Upcoming Angular Events</h1>
   </div>
+  <img src="/assets/images/angular.png"/>
   `
 })
 
