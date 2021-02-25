e5a0e23c3a4912118be30963dc6fb609bdc8555c
Daniel <Daniel.Vaskevic@wealth-dynamix.com>
2021-02-25 14:36:19 +0200

    Clean Up
    Clean Up

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core'
  <div>
   <h1>Upcoming Angular Events</h1>
   <hr/>
-  <div class = "well">
-    <div> Hello World</div>
-  </div>
   <event-thumbnail [eventOtherOne]="event1"></event-thumbnail>
   </div>
 `
